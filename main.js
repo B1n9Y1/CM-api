77334384f06907eae07a795edc4d6c449440845d
B1n9Y1 <90545066+B1n9Y1@users.noreply.github.com>
2022-06-29 14:38:38 +0800

    Delete main.js

--- main.js
+++
@@ -1,4 +0,0 @@
-var myURL = new URL('http://page.sisubeny.top/index.html?key=a')
-console.log(myURL.search);
-var searchParams = new URLSearchParams(myURL.search);
-console.log(searchParams.get('key'));
