d6e9677bd0de93eb223a4b41802afbbece48f78c
B1n9Y1 <B1n9Y1@outlook.com>
2022-06-29 14:32:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 var myURL = new URL('http://page.sisubeny.top/index.html?key=a')
 console.log(myURL.search);
+var searchParams = new URLSearchParams(myURL.search);
+console.log(searchParams.get('x'));
+console.log(searchParams.get('a'));
