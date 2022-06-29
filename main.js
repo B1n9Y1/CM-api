46bcab685646143306dd67faacfd73b6dad1f094
B1n9Y1 <B1n9Y1@outlook.com>
2022-06-29 19:26:56 +0800

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-var myURL = new URL('http://page.sisubeny.top/index.html?key=a')
-console.log(myURL.search);
-var searchParams = new URLSearchParams(myURL.search);
-console.log(searchParams.get('x'));
-console.log(searchParams.get('a'));
-console.log("test")
